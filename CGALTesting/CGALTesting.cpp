e9864f04c971b6b615c5d902a7ccdb9a70bcee41
Tan Hui Yang <44188809+Teruhii@users.noreply.github.com>
2022-08-04 21:17:12 +0800

    Added basic CMake project with only CGAL linked
    Added basic CMake project configuration with only CGAL linked.
    Prerequisites to use include: Install CMake, Install CGAL via vspkg, Edit the location of the vspkg.cmake file under every variable/cachevariable inside the CMakePresets.json file.

---
+++ CGALTesting/CGALTesting.cpp
@@ -0,0 +1,21 @@
+﻿// plsworkthistime.cpp : Defines the entry point for the application.
+//
+
+#include "CGALTesting.h"
+
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/draw_surface_mesh.h>
+#include <fstream>
+typedef CGAL::Simple_cartesian<double>                       Kernel;
+typedef Kernel::Point_3                                      Point;
+typedef CGAL::Surface_mesh<Point>                            Mesh;
+
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
